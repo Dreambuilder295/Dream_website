9bd12142a694cdcaff7b7b6c3249535259c9ee03
Dreambuilder295 <Sumeetmalgotra23@gmail.com>
2025-08-02 21:09:26 +0530

    Initial commit with all files

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
+import os
+import config  # This should be at the top with other imports
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', site_title=config.SITE_TITLE)
 
 @app.route('/about')
 def about():
@@ -16,14 +18,13 @@ def contact():
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
+        # You can use these values to send emails or store in a DB later
         return render_template('thankyou.html', name=name)
     return render_template('contact.html')
 
 @app.route('/quotes')
 def quotes():
     return render_template('quotes.html')
-
-import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
---
+++ config.py
@@ -0,0 +1,3 @@
+SITE_TITLE = "Dream Website"
+OWNER_NAME = "Your Name Here"
+CONTACT_EMAIL = "your@email.com"
